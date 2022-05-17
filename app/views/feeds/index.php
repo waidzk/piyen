3d82ae152dc7ec5a1cbd21ddafbbce1c456047bd
waidzk <waidzk@gmail.com>
2022-05-17 22:16:35 +0700

    up frontend dashboard belum fix (vote, pp comment, hr comment)

--- app/views/feeds/index.php
+++ app/views/feeds/index.php
@@ -1,72 +1,129 @@
-<div class="static ml-12 p-8">
-<h2>Ini adalah halaman feeds</h2>
-<br>
-<form action="<?= BASEURL;?>feeds/add" method="post" enctype="multipart/form-data">
-    <textarea type="text" name="feedValue" id="value_articles" placeholder="Tulis feed..." required></textarea><br>
-    <input type="file" name="feedPhoto" id="feed_photo"><br>
-    <button type="submit" name="make">Buat</button>
-</form>
-<br>
-<table >
+<section class="static flex flex-row p-5 md:ml-16">
+  <div class="md:w-3/4 mb-11 md:mb-0 w-full md:p-3 p-1">
+    <div class="add-feed w-full flex justify-center items-center p-2 border-b">
+      <form action="<?= BASEURL;?>feeds/add" method="post" enctype="multipart/form-data">
+        <textarea type="text" name="feedValue" id="value_articles" class="p-3 w-full focus:border-none" placeholder="Tell us what ur problem .." required></textarea><br>
+        <div class="action-add-feed flex flex-row">
+          <input type="file" name="feedPhoto" id="feed_photo" class="block w-full text-sm text-slate-500
+          file:mr-4 file:py-2 file:px-4
+          file:rounded-full file:border-0
+          file:text-sm file:font-semibold
+          file:bg-pink-50 file:text-pink-500
+          hover:file:bg-pink-100">
+          <button type="submit" name="make" class="bg-pink-500 w-40 rounded-full text-white font-semibold">Buat</button>
+        </div>
+      </form>
+    </div>
+    
     <!-- Menampilkan feed -->
     <?php foreach($data['feeds'] as $feed) : ?>
-        <tr>
-            <td>
-                <hr>
-                <span >
-                    <a href="<?= BASEURL?>profiles/user/<?= $feed['username'];?>/<?= $feed['user_id'];?>">
-                        <!-- foto profile feed -->
-                        <img src="<?= BASEURL;?>app/assets/img/profiles/<?= $feed['photo']; ?>" alt="Profile_Photo" width="30px">
-                        <!-- username feed -->
-                        <h3><b><?= $feed['username']; ?></b></h3>
-                    </a>
-                </span>
-                <!-- Menampilkan isi feed -->
-                <p><?= $feed['feed_value']; ?></p>
-                <!-- Menampilkan lampiran foto feed jika ada -->
-                    <?php if(isset($feed['feed_photo']) && $feed['feed_photo'] !== ''){
-                        echo '<img src="'.BASEURL.'app/assets/img/feeds/'.$feed['feed_photo'].'" alt="Feeds_Photo" width="300px">';
+    <div class="feeds m-4 border-b">
+        <div class="feed">
+            <a href="<?= BASEURL?>profiles/user/<?= $feed['username'];?>/<?= $feed['user_id'];?>" class="flex flex-row items-center w-fit">
+                <div class="feed-pp w-12 mr-2"><img class="rounded-full" src="<?= BASEURL;?>app/assets/img/profiles/<?= $feed['photo']; ?>" alt="Photo Profile"></div>
+                <div class="feed-username text-sm font-semibold"><h3><?= $feed['username']; ?></h3></div>
+            </a>
+            <div class="feed-value p-2 mx-6 w-3/4 border-l-2">
+              <p class="ml-6 text-sm"><?= $feed['feed_value']; ?></p>
+              <?php if(isset($feed['feed_photo']) && $feed['feed_photo'] !== ''){
+                        echo '<img class="ml-6" src="'.BASEURL.'app/assets/img/feeds/'.$feed['feed_photo'].'" alt="Feeds_Photo" width="300px">';
                     }
-                    ?>
-                <!-- Menampilkan waktu untuk feed -->
-                <p><i><?= time_elapsed_string($feed['created_at']); ?></i></p>
+                ?>
+              <p class="ml-6 text-xs text-slate-400">Created <?= time_elapsed_string($feed['created_at']); ?> ago</p>
+            </div>
+        </div>
+        <div class="feed-comments my-1">
+            <h4 class="mx-4 text-xs text-slate-400">Comments</h4>
+            <?php foreach($data['comments'] as $comment): ?>
+                <?php if($feed['id'] == $comment['feed_id']): ?>
+            <div class="comment">
+              <a href="" class="flex flex-row items-center mx-2 my-1 w-fit">
+              <div class="comment-pp w-8 mr-2"><img class="rounded-full" src="https://images.unsplash.com/photo-1597872849945-a1a1ef549c6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="Photo Profile"></div>    
+                <div class="comment-username text-xs font-semibold"><h3><?= $comment['username'] ?></h3></div>
+              </a>
+              <div class="comment-value mx-7 w-3/4 text-xs">
+                <p class="ml-5"><?= $comment['comment_value']; ?></p>
+                <p class="ml-5 text-slate-400">Created <?= time_elapsed_string($comment['created_at']); ?> ago</p>
+              </div>
+              <div class="vote-comment">
+                  
+                <?php 
+			    	if (validate_vote($_SESSION['id'], $comment['id'])) {
+			    		echo '
+			    		<button value="'.$comment['id'].'" id="vote" class="flex flex-row items-center mx-11 my-1 w-fit p-2 rounded-full stroke-white text-white voted">
+                          <div class="voted-icon"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11" />
+                          </svg></div>
+                          <div class="voted-count"><p class="text-xs">voted</p></div>
+                        </button>
+			    		';
+			    	} else {
+			    		echo '
+			    		<button value="'.$comment['id'].'" id="vote" class="flex flex-row items-center mx-11 my-1 w-fit p-2 rounded-full vote">
+                          <div class="voted-icon"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11" />
+                          </svg></div>
+                          <div class="voted-count"><p class="text-xs">vote</p></div>
+                        </button>
+			    		';
+			    	}
+			    ?>
+                <span id="show_vote<?= $comment['id'] ?>">
+                    <p class="mx-5 text-xs text-slate-400">
+			        	<?= showVotes($comment['id']) ?> voted
+                    </p>
+                </span>
                 <hr>
-                <?php foreach($data['comments'] as $comment): ?>
-                    <?php if($feed['id'] == $comment['feed_id']): ?>
-                        <!-- Memunculkan daftar komen dari feed -->
-                        <p>
-                            <b><?= $comment['username'] ?>: </b>
-                            <?= $comment['comment_value']; ?>  | 
-                            <?= time_elapsed_string($comment['created_at']); ?>
-                        </p>
-                        <!-- Bagian Vote -->
-						<?php 
-						if (validate_vote($_SESSION['id'], $comment['id'])) {
-							echo '
-							<button value="'.$comment['id'].'" class="voted" id="vote">❤</button>
-							';
-						} else {
-							echo '
-							<button value="'.$comment['id'].'" class="vote" id="vote">Vote</button>
-							';
-						}
-						?>
-                        <span id="show_vote<?= $comment['id'] ?>">
-							<?= showVotes($comment['id']) ?>
-						</span>
-                    <?php endif; ?>
-                <?php endforeach; ?>
-                <!-- form untuk menambahkan komentar -->
-                <form action="<?= BASEURL?>comments/add/<?= $feed['id']; ?>" method="post">
-                    <input type="text" name="comment" id="comment" placeholder="Comment">
-                    <button type="submit" name="send">Send</button>
-                </form>
-                <hr>
-            </td>
-        </tr>
+              </div>
+            </div>
+                <?php endif; ?>
+            <?php endforeach; ?>
+            <form class="mx-4 text-xs" action="<?= BASEURL?>comments/add/<?= $feed['id']; ?>" method="post">
+                    <input class="w-3/4 p-3 mr-1" type="text" name="comment" id="comment" placeholder="Comment here ...">
+                    <button class="bg-pink-50 p-2 rounded-full text-pink-500 font-semibold" type="submit" name="send">Send</button>
+            </form>
+        </div>
+    </div>
     <?php endforeach; ?>
-</table>
-</div>
+  </div>
+
+  <div class="articles hidden lg:flex flex-col fixed top-0 right-0 m-auto w-1/4 h-screen p-5 border-l-2">
+    <h1 class="text-xl font-bold">Trending Articles</h1>
+    <a href="">
+      <div class="article-card p-4 rounded-md shadow-md">
+        <div class="article-card-title"><h1 class="text-lg font-semibold">Untitled</h1></div>
+        <div class="article-card-value"><p class="text-xs">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex tempore earum praesentium dolorem incidunt, velit autem.</p></div>
+        <div class="footer-article-card text-xs mt-2 flex flex-row justify-between text-slate-400">
+          <span>Created by lia</span>
+          <span>03 May 2022</span>
+        </div>
+      </div>
+    </a>
+    <a href="">
+      <div class="article-card p-4 rounded-md shadow-md">
+        <div class="article-card-title"><h1 class="text-lg font-semibold">Untitled</h1></div>
+        <div class="article-card-value"><p class="text-xs">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex tempore earum praesentium dolorem incidunt, velit autem.</p></div>
+        <div class="footer-article-card text-xs mt-2 flex flex-row justify-between text-slate-400">
+          <span>Created by lia</span>
+          <span>03 May 2022</span>
+        </div>
+      </div>
+    </a>
+    <a href="">
+      <div class="article-card p-4 rounded-md shadow-md">
+        <div class="article-card-title"><h1 class="text-lg font-semibold">Untitled</h1></div>
+        <div class="article-card-value"><p class="text-xs">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex tempore earum praesentium dolorem incidunt, velit autem.</p></div>
+        <div class="footer-article-card text-xs mt-2 flex flex-row justify-between text-slate-400">
+          <span>Created by lia</span>
+          <span>03 May 2022</span>
+        </div>
+      </div>
+    </a>
+    <a href="" class="arrow flex flex-row items-center justify-end mt-10"><p>More </p><span><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+    </svg></span></a>
+  </div>
+</section>
 <script src="jquery-3.1.1.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script type="text/javascript">
--- app/views/template/navigator.php
+++ app/views/template/navigator.php
@@ -9,9 +9,9 @@
     <div class="navigation-section bg-white flex flex-row md:flex-col items-center justify-center fixed px-10 md:px-3 inset-x-0 bottom-0 md:inset-y-0 md:bottom-0 md:left-0 md:inset-x-auto border-t-2 md:border-r-2">
         <div class="head-navigation">
           <?php if (isset($_SESSION['login'])):?>
-            <a href="<?= BASEURL;?>profiles/user/<?= $_SESSION['username'] ?>/<?= $_SESSION['id'] ?>" class="photo-profile w-8 p-3"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-              </svg></a>
+            <a href="<?= BASEURL;?>profiles/user/<?= $_SESSION['username'] ?>/<?= $_SESSION['id'] ?>">
+              <img class="photo-profile w-10 rounded-full" src="<?= BASEURL;?>app/assets/img/profiles/<?= isset($data['userdata']['photo'])? $data['userdata']['photo'] : 'user.jpg'; ?>" alt="">
+            </a>
           <?php endif; ?>
         </div>
         <div class="navigation flex flex-row md:flex-col items-center justify-center w-12 mx-auto md:my-12">
@@ -24,8 +24,8 @@
             <a href="<?= BASEURL;?>articles/add" class="nav-item mx-3 md:my-4 <?php if (exploded($_SERVER['REQUEST_URI'])[2]=='articles') {echo 'active'; }?>"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
               </svg></a>
-            <a href="#voted" class="nav-item mx-3 md:my-4 <?php if (exploded($_SERVER['REQUEST_URI'])[0]=='voted') {echo 'active'; }?>"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11" />
+            <a href="#articles" class="nav-item mx-3 md:my-4 <?php if (exploded($_SERVER['REQUEST_URI'])[0]=='voted') {echo 'active'; }?>"><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
               </svg></a>
         </div>
         <div class="foot-navigation">
